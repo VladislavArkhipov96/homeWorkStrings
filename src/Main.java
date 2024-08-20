90146aca9b650e04b1fee96b7af75c4e017d5d85
VladislavArkhipov96 <y906ee@mail.ru>
2024-08-20 23:45:57 +0300

    Сделанно ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,27 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        //Task 1
+        System.out.println("Task 1");
 
+        String firstName = "ivan";
+        String middleName = "Ivanovich";
+        String lastName = "ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //Task 2
+        System.out.println("Task 2");
+
+        String fullNameInUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameInUpperCase);
+
+        //Task 3
+        System.out.println("Task 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
